78837acf57f1e106557a7c10bcb05ef3f5f31ada
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2023-01-19 17:58:43 +0300

    init

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let newArr = []
+
+  for(let i = 0; i < matrix.length; i++) {
+		for(let item of matrix[i]) {
+    	newArr.push(item)
+    }
+  }
+  return newArr.sort((a, b) => a-b)
 }
